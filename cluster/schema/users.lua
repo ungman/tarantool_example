2068e6ffbe808da62b9cd1042c55a97f55808e0a
myungman <myungman@phoenixit.ru>
2024-01-24 11:33:01 +0300

    add fix incorrect users on tarantool

--- cluster/schema/users.lua
+++ cluster/schema/users.lua
@@ -1,6 +1,6 @@
-local user = {}
+local users = {}
 
-function articles.init()
+function  users.init()
     local space = box.schema.space.create("users", { if_not_exists = true })
     space:format({
         { name = "login", type = "string" },
@@ -12,4 +12,4 @@ function articles.init()
                                     if_not_exists = true })
 end
 
-return articles
+return  users
